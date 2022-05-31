57651f0ebe13e59ed4be3683f0de37fcc77214a2
Lauri Miettinen <lauri.miettinen90@gmail.com>
2022-05-31 19:40:33 +0300

    Began making the Fortress Riddle

--- three-alien-overlords/solution.js
+++ three-alien-overlords/solution.js
@@ -1,6 +1,32 @@
 
 const logger = new Logger();
 
+
+const obviousQuestion = {
+	id: 0,
+	question: 'Is the sky blue?',
+	description: 'An obvious question that you know the answer to.'
+}
+
+const referenceQuestion = {
+	id: 1,
+	question: 'Was the answer to the last question a yes?',
+	description: "Ask an overlord information on another's answer."
+}
+
+const answerComparisonQuestion = {
+	id: 2,
+	question: 'Were the last two answers a yes?',
+	description: 'Asks the overlord to compare two answers'
+}
+
+
+const randomOverlordIdentityQuestion = {
+	id:3,
+	question: 'Is the first in line the Overlord who answers randomly?',
+	description: 'Used to determine the identity of the randomly answering one.'
+}
+
 $(document).ready(function(){
 
 	$("#run-button").click(() => {
@@ -92,7 +118,7 @@ const runSolution = (printFunction = ()=>{}) => {
 		wordForfalse
 	}
 	
-	getSolution2({overlord1, overlord2, overlord3, idolOfTee, idolOfEff, idolOfArr, questionsAndAnswers, printFunction});
+	getSolution1({overlord1, overlord2, overlord3, idolOfTee, idolOfEff, idolOfArr, questionsAndAnswers, printFunction});
 	
 	printFunction('<br><p>Idols presented. What is your verdict, o\' Overlords?..</p><br><br><br><br><br>');
 	
