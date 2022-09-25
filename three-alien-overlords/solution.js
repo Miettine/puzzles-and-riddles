fe425ecb703a671472ed02144a5607cb20d8df1b
Lauri Miettinen <lauri.miettinen90@gmail.com>
2022-09-25 16:18:29 +0300

    Revived the solution with 46% success rate by getting it from version control.

--- three-alien-overlords/solution.js
+++ three-alien-overlords/solution.js
@@ -47,6 +47,26 @@ $(document).ready(function(){
 
 		setTimer($('#timer'), t1 - t0);
 	});
+	$("#run-best-button").click(() => {
+		const t0 = performance.now();
+		const solutionLogElement = $("#solution-log");
+
+		solutionLogElement.text("");
+		$("#analysis").text("");
+
+		//$('#note-text').html('<b>Scroll all the way down to view the whole solution.</b>');
+
+		runFortySixPercentSolution(getBestSolutionMapping(), (logText) =>
+		solutionLogElement.append(`${logText} <br><br>`)
+		);
+
+		//console.log(logger.getLog());
+		console.log(logger.getAnswersToOverlordsMap());
+
+		const t1 = performance.now();
+
+		setTimer($("#timer"), t1 - t0);
+    });
 	$("#run-many-times-button").click(() => {
 		logger.clear();
 
@@ -82,7 +102,72 @@ $(document).ready(function(){
 
 		setTimer($("#timer"), t1 - t0);
 	});
+
+	$("#run-best-solution-button").click(() => {
+		logger.clear();
+
+		const t0 = performance.now();
+
+		const mapping = getBestSolutionMapping();
+
+		console.log("The mapping is: " + mapping);
+
+		for (let i = 0; i < 10000; i++) {
+			runFortySixPercentSolution(mapping);
+		}
+
+		const numberOfGames = logger.getNumberOfGames();
+		const numberOfWonGames = logger.getNumberOfWonGames();
+
+		$("#solution-log").html(
+			`The mapping is: ${mapping} <br>
+			<br>
+			games: ${numberOfGames}, won: ${numberOfWonGames}, winrate: ${(numberOfWonGames / numberOfGames) * 100}%`
+		);
+
+		const map = logger.getAnswersToOverlordsMap();
+
+		console.log(map);
+
+		const t1 = performance.now();
+
+		setTimer($("#timer"), t1 - t0);
+	});
+	$("#run-best-solution-with-random-mapping-button").click(() => {
+		logger.clear();
+
+		const t0 = performance.now();
+
+		const mapping = getRandomizedMapping();
+
+		console.log("The mapping is: " + mapping);
+
+		for (let i = 0; i < 10000; i++) {
+			runFortySixPercentSolution(mapping);
+		}
+
+		const numberOfGames = logger.getNumberOfGames();
+		const numberOfWonGames = logger.getNumberOfWonGames();
+
+		$("#solution-log").html(
+		`The mapping is: ${mapping} <br>
+			<br>
+			games: ${numberOfGames}, won: ${numberOfWonGames}, winrate: ${
+			(numberOfWonGames / numberOfGames) * 100
+		}%`
+		);
+
+		const map = logger.getAnswersToOverlordsMap();
+
+		console.log(map);
+
+		const t1 = performance.now();
+
+		setTimer($("#timer"), t1 - t0);
+    });
 });
+
+const getBestSolutionMapping = () => [2, 4, 1, 0, 5, 1, 4, 3];
 
 const setTimer = (timerElement, time) => {
 	timerElement.html(`The program took ${time} milliseconds to run.<br>`);
@@ -98,6 +183,62 @@ function getRandomInt(max) {
 const runSolution = (printFunction = ()=>{}) => {
 	runSolutionWithMapping([2, 1, 0, 5, 5, 0, 1, 2], printFunction);
 }
+
+const runFortySixPercentSolution = (mapping = [2, 4, 1, 0, 5, 1, 4, 3], printFunction = () => {}) => {
+	printFunction("Starting program..");
+
+	const wordPronunciations = shuffle(["Ozo", "Ulu"]);
+
+	const wordForTrue = new Word(wordPronunciations[0], true);
+	const wordForfalse = new Word(wordPronunciations[1], false);
+
+	const overlords = shuffle([
+		new TruthfulOverlord(0, "Tee", wordForTrue, wordForfalse),
+		new LyingOverlord(1, "Eff", wordForTrue, wordForfalse),
+		new RandomOverlord(2, "Arr", wordForTrue, wordForfalse),
+	]);
+
+	const idolOfTee = new Idol(0, "Idol of Tee");
+	const idolOfEff = new Idol(1, "Idol of Eff");
+	const idolOfArr = new Idol(2, "Idol of Arr");
+
+	const [overlord1, overlord2, overlord3] = overlords;
+
+	const questionsAndAnswers = [];
+
+	const gameLog = {
+	questionsAndAnswers,
+	overlords,
+	wordForTrue,
+	wordForfalse,
+	};
+
+	getFortySixPercentSolution({
+		overlord1,
+		overlord2,
+		overlord3,
+		idolOfTee,
+		idolOfEff,
+		idolOfArr,
+		questionsAndAnswers,
+		printFunction,
+	}, mapping);
+
+	printFunction(
+	"<br><p>Idols presented. What is your verdict, o' Overlords?..</p><br><br><br><br><br>"
+	);
+
+	printFunction(overlord1.getOpinion());
+	printFunction(overlord2.getOpinion());
+	printFunction(overlord3.getOpinion());
+
+	gameLog.victory =
+	overlord1.isPleased() && overlord2.isPleased() && overlord3.isPleased();
+
+	logger.addLog(gameLog);
+
+	printFunction("Program finished.");
+};
 
 const runSolutionWithMapping = (mapping, printFunction = () => {}) => {
 	printFunction("Starting program..");
@@ -222,6 +363,126 @@ const getSolution2 = ({overlord1, overlord2, overlord3, idolOfTee, idolOfEff, id
 	mapping
   );
 }
+
+const getFortySixPercentSolution = (
+	{
+	overlord1,
+	overlord2,
+	overlord3,
+	idolOfTee,
+	idolOfEff,
+	idolOfArr,
+	questionsAndAnswers,
+	printFunction,
+	}, mapping = [2, 4, 1, 0, 5, 1, 4, 3]) => {
+	printFunction(obviousQuestion.question);
+
+	const anwerOfLord1Question1 = overlord1.getAnswer(obviousQuestion.id, null);
+	printFunction(`Overlord 1: ${anwerOfLord1Question1.getPronunciation()}`);
+
+	questionsAndAnswers.push({
+	question: obviousQuestion,
+	orderNumber: 0,
+	addressedTo: 0,
+	answer: anwerOfLord1Question1.getPronunciation(),
+	});
+
+	printFunction(referenceQuestion.question);
+	const anwerOfLord2Question2 = overlord2.getAnswer(referenceQuestion.id, {
+		wordReference1: anwerOfLord1Question1,
+	});
+	printFunction(`Overlord 2: ${anwerOfLord2Question2.getPronunciation()}`);
+
+	questionsAndAnswers.push({
+		question: referenceQuestion,
+		orderNumber: 1,
+		addressedTo: 1,
+		answer: anwerOfLord2Question2.getPronunciation(),
+	});
+	let answerOfLord1Question3;
+	let answerOfLord2Question3;
+	let answerOfLord3Question3;
+
+	if (anwerOfLord1Question1.getPronunciation() == anwerOfLord2Question2.getPronunciation()){
+
+		printFunction(answerComparisonQuestion.question);
+
+		const references = {wordReference1: anwerOfLord1Question1, wordReference2:anwerOfLord2Question2};
+
+		answerOfLord1Question3 = overlord1.getAnswer(answerComparisonQuestion.id, references)
+		printFunction(`Overlord 1: ${answerOfLord1Question3.getPronunciation()}`);
+
+		answerOfLord2Question3 = overlord2.getAnswer(answerComparisonQuestion.id, references)
+		printFunction(`Overlord 2: ${answerOfLord2Question3.getPronunciation()}`);
+
+		answerOfLord3Question3 = overlord3.getAnswer(answerComparisonQuestion.id, references)
+		printFunction(`Overlord 3: ${answerOfLord3Question3.getPronunciation()}`);
+		questionsAndAnswers.push({
+			question: answerComparisonQuestion,
+			orderNumber: 2,
+			addressedTo: 0,
+			answer: answerOfLord1Question3.getPronunciation()
+		},
+		{
+			question: answerComparisonQuestion,
+			orderNumber: 2,
+			addressedTo:1,
+			answer: answerOfLord2Question3.getPronunciation()
+		},
+		{
+			question: answerComparisonQuestion,
+			orderNumber: 2,
+			addressedTo:2,
+			answer: answerOfLord3Question3.getPronunciation()
+		});
+	} else { 
+		printFunction(referenceQuestion.question);
+
+		const references = {wordReference1: anwerOfLord2Question2};
+
+		answerOfLord1Question3 = overlord1.getAnswer(referenceQuestion.id, references)
+		printFunction(`Overlord 1: ${answerOfLord1Question3.getPronunciation()}`);
+
+		answerOfLord2Question3 = overlord2.getAnswer(referenceQuestion.id, references)
+		printFunction(`Overlord 2: ${answerOfLord2Question3.getPronunciation()}`);
+
+		answerOfLord3Question3 = overlord3.getAnswer(referenceQuestion.id, references)
+		printFunction(`Overlord 3: ${answerOfLord3Question3.getPronunciation()}`);
+
+		questionsAndAnswers.push({
+			question: answerComparisonQuestion,
+			orderNumber: 2,
+			addressedTo: 0,
+			answer: answerOfLord1Question3.getPronunciation()
+		},
+		{
+			question: answerComparisonQuestion,
+			orderNumber: 2,
+			addressedTo:1,
+			answer: answerOfLord2Question3.getPronunciation()
+		},
+		{
+			question: answerComparisonQuestion,
+			orderNumber: 2,
+			addressedTo:2,
+			answer: answerOfLord3Question3.getPronunciation()
+		});
+	}
+
+	presentIdolsBasedOnAnswers(
+	{
+	  anwerOfLord1Question1,
+	  anwerOfLord2Question2,
+	  answerOfLord3Question3,
+	  overlord1,
+	  overlord2,
+	  overlord3,
+	  idolOfTee,
+	  idolOfEff,
+	  idolOfArr,
+	},
+	mapping);
+};
 
 /**
  * 
